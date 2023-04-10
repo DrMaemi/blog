0e4cfbf9f88ad59cdc006fb3d528e9fb7bf5e3de
DrMaemi <dev.maemi@gmail.com>
2023-04-10 11:37:31 +0000

    Deploying to gh-pages from @ DrMaemi/blog@6fa7995422336b436290bdedb14df6f55dc9a181 🚀

---
+++ assets/04-git-branch-pull-request.html.1e83ff83.js
@@ -0,0 +1,57 @@
+import{D as r}from"./DetailsOpen.d5ab58f9.js";import{f as i,o as s,c as n,a as e,b as u,d as t,u as l,e as a,r as d}from"./app.536ff352.js";import{_ as c}from"./_plugin-vue_export-helper.cdc0426e.js";const B=a(`<h2 id="\u110B\u116F\u11AB\u1100\u1167\u11A8-\u110C\u1165\u110C\u1161\u11BC\u1109\u1169\u110B\u1174-\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u110C\u1165\u11BC\u1107\u1169-\u1107\u1169\u1100\u1175" tabindex="-1"><a class="header-anchor" href="#\u110B\u116F\u11AB\u1100\u1167\u11A8-\u110C\u1165\u110C\u1161\u11BC\u1109\u1169\u110B\u1174-\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u110C\u1165\u11BC\u1107\u1169-\u1107\u1169\u1100\u1175" aria-hidden="true">#</a> \uC6D0\uACA9 \uC800\uC7A5\uC18C\uC758 \uBE0C\uB79C\uCE58 \uC815\uBCF4 \uBCF4\uAE30</h2><div class="language-text ext-text"><pre class="language-text"><code>git remote update
+git branch -r
+</code></pre></div><h2 id="\u110B\u116F\u11AB\u1100\u1167\u11A8-\u110C\u1165\u110C\u1161\u11BC\u1109\u1169\u110B\u1174-\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u1100\u1161\u110C\u1167\u110B\u1169\u1100\u1175" tabindex="-1"><a class="header-anchor" href="#\u110B\u116F\u11AB\u1100\u1167\u11A8-\u110C\u1165\u110C\u1161\u11BC\u1109\u1169\u110B\u1174-\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u1100\u1161\u110C\u1167\u110B\u1169\u1100\u1175" aria-hidden="true">#</a> \uC6D0\uACA9 \uC800\uC7A5\uC18C\uC758 \uBE0C\uB79C\uCE58 \uAC00\uC838\uC624\uAE30</h2><p>\uB9CC\uC57D /feature/created-branch \uBE0C\uB79C\uCE58\uB97C \uAC00\uC838\uC624\uACE0 \uC2F6\uB2E4\uBA74</p><div class="language-text ext-text"><pre class="language-text"><code>git checkout -t origin/feature/created-branch
+</code></pre></div><h2 id="\u1105\u1169\u110F\u1165\u11AF\u110B\u1166-\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u1109\u1162\u11BC\u1109\u1165\u11BC" tabindex="-1"><a class="header-anchor" href="#\u1105\u1169\u110F\u1165\u11AF\u110B\u1166-\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u1109\u1162\u11BC\u1109\u1165\u11BC" aria-hidden="true">#</a> \uB85C\uCEEC\uC5D0 \uBE0C\uB79C\uCE58 \uC0DD\uC131</h2><div class="language-text ext-text"><pre class="language-text"><code>git branch feature/test
+</code></pre></div><h2 id="\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u110C\u1165\u11AB\u1112\u116A\u11AB" tabindex="-1"><a class="header-anchor" href="#\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u110C\u1165\u11AB\u1112\u116A\u11AB" aria-hidden="true">#</a> \uBE0C\uB79C\uCE58 \uC804\uD658</h2><div class="language-text ext-text"><pre class="language-text"><code>git checkout feature/test
+</code></pre></div><h3 id="\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u1109\u1162\u11BC\u1109\u1165\u11BC\u1100\u116A-\u1103\u1169\u11BC\u1109\u1175\u110B\u1166-\u110C\u1165\u11AB\u1112\u116A\u11AB" tabindex="-1"><a class="header-anchor" href="#\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u1109\u1162\u11BC\u1109\u1165\u11BC\u1100\u116A-\u1103\u1169\u11BC\u1109\u1175\u110B\u1166-\u110C\u1165\u11AB\u1112\u116A\u11AB" aria-hidden="true">#</a> \uBE0C\uB79C\uCE58 \uC0DD\uC131\uACFC \uB3D9\uC2DC\uC5D0 \uC804\uD658</h3><div class="language-text ext-text"><pre class="language-text"><code>git checkout -b feature/test
+</code></pre></div><h2 id="\u1105\u1169\u110F\u1165\u11AF-\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u110C\u1161\u11A8\u110B\u1165\u11B8-\u1102\u1162\u110B\u116D\u11BC\u110B\u1173\u11AF-\u110B\u116F\u11AB\u1100\u1167\u11A8-\u110C\u1165\u110C\u1161\u11BC\u1109\u1169\u110B\u1166-push" tabindex="-1"><a class="header-anchor" href="#\u1105\u1169\u110F\u1165\u11AF-\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u110C\u1161\u11A8\u110B\u1165\u11B8-\u1102\u1162\u110B\u116D\u11BC\u110B\u1173\u11AF-\u110B\u116F\u11AB\u1100\u1167\u11A8-\u110C\u1165\u110C\u1161\u11BC\u1109\u1169\u110B\u1166-push" aria-hidden="true">#</a> \uB85C\uCEEC \uBE0C\uB79C\uCE58 \uC791\uC5C5 \uB0B4\uC6A9\uC744 \uC6D0\uACA9 \uC800\uC7A5\uC18C\uC5D0 push</h2><div class="language-text ext-text"><pre class="language-text"><code>git push &lt;remote alias&gt; &lt;branch&gt;
+</code></pre></div><details class="custom-container details"><summary>\uC608\uC81C - test-file \uC0DD\uC131, \uCEE4\uBC0B, \uD478\uC2DC</summary><div class="language-text ext-text"><pre class="language-text"><code>$ touch test-file
+$ git commit -m &quot;Create file: test-file&quot;
+[feature/test b3003ae] Create file: test-file
+ 1 file changed, 0 insertions(+), 0 deletions(-)
+ create mode 100644 test-file
+$ git push origin feature/test
+Enumerating objects: 4, done.
+Counting objects: 100% (4/4), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 279 bytes | 279.00 KiB/s, done.
+Total 3 (delta 1), reused 1 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+remote:
+remote: Create a pull request for &#39;feature/test&#39; on GitHub by visiting:
+remote:      https://oss.navercorp.com/sh-maemi-lee/git-study/pull/new/feature/test
+remote:
+To https://oss.navercorp.com/sh-maemi-lee/git-study.git
+ * [new branch]      feature/test -&gt; feature/test
+</code></pre><div class="highlight-lines"><div class="highlight-line">\xA0</div><div class="highlight-line">\xA0</div><br><br><br><div class="highlight-line">\xA0</div><br><br><br><br><br><br><br><br><br><br><br><br><br></div></div></details><p>\uC791\uC5C5 \uB0B4\uC6A9 \uC5C6\uC774 \uBE0C\uB79C\uCE58 \uC0DD\uC131 \uD6C4 \uC6D0\uACA9 \uC800\uC7A5\uC18C\uC5D0 push\uD560 \uC218\uB3C4 \uC788\uC2B5\uB2C8\uB2E4.</p><h2 id="pull-request-comment-merge" tabindex="-1"><a class="header-anchor" href="#pull-request-comment-merge" aria-hidden="true">#</a> Pull request &amp; Comment &amp; Merge</h2>`,16),o={href:"https://github.com",target:"_blank",rel:"noopener noreferrer"},h=a(`<p class="align-center"><img src="https://drive.google.com/uc?export=view&amp;id=1eZn83h9Elu_rLMe6yfK9fCTWUk28E_fB" alt="" loading="lazy"> &lt;Github - Pull request &amp; Comment &amp; Merge&gt;</p><p>Pull request, Comment, Merge\uB97C \uBAA8\uB450 \uB05D\uB0C8\uB2E4\uBA74 \uB85C\uCEEC \uC800\uC7A5\uC18C\uC640 \uC6D0\uACA9 \uC800\uC7A5\uC18C\uC758 \uC0C1\uD0DC\uB97C \uB3D9\uAE30\uD654\uD574\uC90D\uB2C8\uB2E4.</p><div class="language-text ext-text"><pre class="language-text"><code>$ git branch
+* feature/test
+  master
+$ git checkout master
+Switched to branch &#39;master&#39;
+Your branch is up to date with &#39;origin/master&#39;.
+$ git remote update
+remote: Enumerating objects: 1, done.
+remote: Counting objects: 100% (1/1), done.
+remote: Total 1 (delta 0), reused 0 (delta 0), pack-reused 0
+Unpacking objects: 100% (1/1), 287 bytes | 287.00 KiB/s, done.
+From https://oss.navercorp.com/sh-maemi-lee/git-study
+   f1daa0c..9c7aadb  master     -&gt; origin/master
+$ git pull
+Updating f1daa0c..9c7aadb
+Fast-forward
+ test-file | 0
+ 1 file changed, 0 insertions(+), 0 deletions(-)
+ create mode 100644 test-file
+</code></pre><div class="highlight-lines"><div class="highlight-line">\xA0</div><br><br><div class="highlight-line">\xA0</div><br><br><div class="highlight-line">\xA0</div><br><br><br><br><br><br><div class="highlight-line">\xA0</div><br><br><br><br><br></div></div><p>\uADF8\uB7EC\uB098 \uB85C\uCEEC\uC5D0\uC11C \uD655\uC778\uD574\uBCF4\uBA74 \uC5EC\uC804\uD788 \uBE0C\uB79C\uCE58 \uC815\uBCF4\uAC00 \uB0A8\uC544\uC788\uC2B5\uB2C8\uB2E4.</p><div class="language-text ext-text"><pre class="language-text"><code>$ git branch
+  feature/test
+* master
+$ git branch -r
+  origin/HEAD -&gt; origin/master
+  origin/feature/test
+  origin/master
+</code></pre><div class="highlight-lines"><div class="highlight-line">\xA0</div><br><br><div class="highlight-line">\xA0</div><br><br><br></div></div><h2 id="\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u1109\u1161\u11A8\u110C\u1166" tabindex="-1"><a class="header-anchor" href="#\u1107\u1173\u1105\u1162\u11AB\u110E\u1175-\u1109\u1161\u11A8\u110C\u1166" aria-hidden="true">#</a> \uBE0C\uB79C\uCE58 \uC0AD\uC81C</h2><p>\uB85C\uCEEC \uBE0C\uB79C\uCE58 \uC0AD\uC81C</p><div class="language-text ext-text"><pre class="language-text"><code>$ git branch -d feature/test
+Deleted branch feature/test (was b3003ae).
+$ git branch
+* master
+</code></pre><div class="highlight-lines"><div class="highlight-line">\xA0</div><br><div class="highlight-line">\xA0</div><br></div></div>`,8),g=i({__name:"04-git-branch-pull-request.html",setup(A){return(p,b)=>{const C=d("ExternalLinkIcon");return s(),n("div",null,[B,e("p",null,[u("\uB85C\uCEEC \uBE0C\uB79C\uCE58 \uC791\uC5C5 \uB0B4\uC6A9\uC744 \uC6D0\uACA9 \uC800\uC7A5\uC18C\uC5D0 push\uD588\uB2E4\uBA74, \uBE0C\uB77C\uC6B0\uC800\uB85C "),e("a",o,[u("Github"),t(C)]),u("\uC5D0 \uC811\uC18D\uD558\uC5EC Pull request\uB97C \uD560 \uC218 \uC788\uC2B5\uB2C8\uB2E4.")]),h,t(l(r))])}}}),v=c(g,[["__file","04-git-branch-pull-request.html.vue"]]);export{v as default};
