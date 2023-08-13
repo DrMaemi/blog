b5aa358197cbcbbfa81af65dd705747cb9505af6
DrMaemi <dev.maemi@gmail.com>
2023-08-13 11:15:13 +0000

    Deploying to gh-pages from @ DrMaemi/blog@cdcc28f2742334cb383dc82a4af1f36dc3ee4569 🚀

---
+++ assets/02-create-vuepress-theme-hope-project.html.744c04c2.js
@@ -0,0 +1,106 @@
+import{D as a}from"./DetailsOpen.d5ab58f9.js";import{f as C,o,c as t,a as u,b as n,d as e,u as p,e as c,r as B}from"./app.9e40aede.js";import{_ as r}from"./_plugin-vue_export-helper.cdc0426e.js";const l=u("h2",{id:"\u1100\u1162\u1107\u1161\u11AF-\u1112\u116A\u11AB\u1100\u1167\u11BC-\u1100\u116E\u1109\u1165\u11BC",tabindex:"-1"},[u("a",{class:"header-anchor",href:"#\u1100\u1162\u1107\u1161\u11AF-\u1112\u116A\u11AB\u1100\u1167\u11BC-\u1100\u116E\u1109\u1165\u11BC","aria-hidden":"true"},"#"),n(" \uAC1C\uBC1C \uD658\uACBD \uAD6C\uC131")],-1),d=u("p",null,"\uC6B0\uC120 Node.js\uB97C \uB2E4\uC6B4\uBC1B\uC544\uC57C \uD569\uB2C8\uB2E4. Node.js\uB294 \uD06C\uB86C\uC758 V8 \uC790\uBC14\uC2A4\uD06C\uB9BD\uD2B8 \uC5D4\uC9C4\uC5D0\uC11C \uBE4C\uB4DC\uB41C \uC790\uBC14\uC2A4\uD06C\uB9BD\uD2B8 \uB7F0\uD0C0\uC784\uC785\uB2C8\uB2E4.",-1),i={href:"https://nodejs.org/en/",target:"_blank",rel:"noopener noreferrer"},D=u("p",null,"\uC774\uD6C4 IDE\uC778 Editor\uB97C \uC124\uCE58\uD574\uC8FC\uC5B4\uC57C \uD558\uB294\uB370, \uC800\uB294 VS Code\uB97C \uC774\uC6A9\uD588\uC2B5\uB2C8\uB2E4.",-1),A={href:"https://code.visualstudio.com/",target:"_blank",rel:"noopener noreferrer"},k=c(`<h3 id="yarn-\u1109\u1165\u11AF\u110E\u1175" tabindex="-1"><a class="header-anchor" href="#yarn-\u1109\u1165\u11AF\u110E\u1175" aria-hidden="true">#</a> yarn \uC124\uCE58</h3><p>yarn\uC744 \uC124\uCE58\uD558\uC9C0 \uC54A\uC544\uB3C4 \uB418\uB294\uB370, \uC124\uCE58\uB97C \uC6D0\uD55C\uB2E4\uBA74 Node\uB97C \uC124\uCE58\uD55C \uB4A4 npm\uC73C\uB85C yarn\uC744 \uC124\uCE58\uD574\uC90D\uB2C8\uB2E4.</p><div class="language-text ext-text"><pre class="language-text"><code>sudo npm install -g yarn
+</code></pre></div><p>sudo \uBA85\uB839\uC5B4 \uC5C6\uC774 <code>npm install -g yarn</code>\uC744 \uC2E4\uD589\uD558\uBA74 <code>npm ERR! Error: EACCES: permission denied, mkdir &#39;/usr/local/lib/node_modules/yarn&#39;</code> \uC5D0\uB7EC\uAC00 \uBC1C\uC0DD\uD588\uC5C8\uB294\uB370 \uC720\uC758\uBC14\uB78D\uB2C8\uB2E4.</p><h2 id="\u1111\u1173\u1105\u1169\u110C\u1166\u11A8\u1110\u1173-\u1109\u1162\u11BC\u1109\u1165\u11BC" tabindex="-1"><a class="header-anchor" href="#\u1111\u1173\u1105\u1169\u110C\u1166\u11A8\u1110\u1173-\u1109\u1162\u11BC\u1109\u1165\u11BC" aria-hidden="true">#</a> \uD504\uB85C\uC81D\uD2B8 \uC0DD\uC131</h2><p>\uC784\uC758\uC758 \uC774\uB984\uC758 \uB514\uB809\uD1A0\uB9AC\uB97C \uC0DD\uC131\uD55C \uB4A4 \uD574\uB2F9 \uB514\uB809\uD1A0\uB9AC\uC5D0\uC11C <code>npm create vuepress-theme-hope@next docs</code> \uBA85\uB839\uC5B4\uB97C \uC2E4\uD589\uD569\uB2C8\uB2E4.</p><div class="custom-container tip"><p class="custom-container-title">\uC704\uCE58 \uC778\uC790</p><p>\uBA85\uB839\uC5B4 \uC911 <code>docs</code>\uC5D0 \uD574\uB2F9\uD558\uB294 \uC778\uC790\uB294 VuePress \uD504\uB85C\uC81D\uD2B8 \uD30C\uC77C\uB4E4\uC744 \uC704\uCE58\uC2DC\uD0AC \uD3F4\uB354 \uC774\uB984\uC744 \uC758\uBBF8\uD558\uBA70, \uC784\uC758\uB85C \uBCC0\uACBD\uC774 \uAC00\uB2A5\uD569\uB2C8\uB2E4. <code>docs</code>\uB97C \uC785\uB825\uD558\uBA74 \uD604\uC7AC \uD504\uB85C\uC81D\uD2B8 \uB8E8\uD2B8 \uB514\uB809\uD1A0\uB9AC \uD558\uC704\uC5D0 <code>docs</code> \uD3F4\uB354\uB97C \uC0DD\uC131\uD558\uACE0 \uD574\uB2F9 \uD3F4\uB354 \uD558\uC704\uC5D0 VuePress \uAD00\uB828 \uD30C\uC77C\uB4E4\uC774 \uC0DD\uC131\uB429\uB2C8\uB2E4.</p><p>\uB9CC\uC57D \uB8E8\uD2B8 \uB514\uB809\uD1A0\uB9AC \uD558\uC704\uC5D0 \uBC14\uB85C VuePress \uD504\uB85C\uC81D\uD2B8 \uD30C\uC77C\uB4E4\uC744 \uC0DD\uC131\uC2DC\uD0A4\uACE0 \uC2F6\uB2E4\uBA74 <code>.</code>\uC744 \uC785\uB825\uD558\uBA74 \uB418\uC9C0\uB9CC, \uAC00\uAE09\uC801\uC774\uBA74 <code>docs</code> \uD3F4\uB354 \uC774\uB984\uC744 \uC0AC\uC6A9\uD558\uB3C4\uB85D VuePress \uACF5\uC2DD \uD648\uD398\uC774\uC9C0\uC5D0\uC11C \uC548\uB0B4\uD558\uACE0 \uC788\uC2B5\uB2C8\uB2E4.</p></div><details class="custom-container details"><summary>\uBA85\uB839\uC5B4 \uC2E4\uD589 \uACFC\uC815</summary><div class="language-bash ext-sh"><pre class="language-bash"><code>$ <span class="token function">npm</span> create vuepress-theme-hope@next docs
+Need to <span class="token function">install</span> the following packages:
+  create-vuepress-theme-hope@2.0.0-beta.110
+Ok to proceed? <span class="token punctuation">(</span>y<span class="token punctuation">)</span>
+? Select a language to display / \u9009\u62E9\u663E\u793A\u8BED\u8A00 english <span class="token punctuation">(</span>US<span class="token punctuation">)</span>
+? Choose package manager <span class="token function">npm</span>
+Generating package.json<span class="token punctuation">..</span>.
+? Your project name blog-project-test
+? Your project version <span class="token number">2.0</span>.0
+? Your project description A project of vuepress-theme-hope
+? Your project license MIT
+? Do you need a GitHub workflow to deploy docs on GitHub pages? Yes
+? What <span class="token builtin class-name">type</span> of project <span class="token keyword">do</span> you want to create? blog
+Generating Template<span class="token punctuation">..</span>.
+Installing Deps<span class="token punctuation">..</span>.
+This may take a few minutes, please be patient.
+We can not correctly output progress bar from child process, so the process may <span class="token function">look</span> stuck.
+
+added <span class="token number">636</span> packages, and audited <span class="token number">637</span> packages <span class="token keyword">in</span> 52s
+
+<span class="token number">104</span> packages are looking <span class="token keyword">for</span> funding
+  run <span class="token variable"><span class="token variable">\`</span><span class="token function">npm</span> fund<span class="token variable">\`</span></span> <span class="token keyword">for</span> details
+
+<span class="token number">12</span> high severity vulnerabilities
+
+To address issues that <span class="token keyword">do</span> not require attention, run:
+  <span class="token function">npm</span> audit fix
+
+To address all issues <span class="token punctuation">(</span>including breaking changes<span class="token punctuation">)</span>, run:
+  <span class="token function">npm</span> audit fix <span class="token parameter variable">--force</span>
+
+Run <span class="token variable"><span class="token variable">\`</span><span class="token function">npm</span> audit<span class="token variable">\`</span></span> <span class="token keyword">for</span> details.
+Successful Generated<span class="token operator">!</span>
+? Would you like to preview template now? No
+Hint: You should execute <span class="token string">&quot;npm run docs:dev&quot;</span> to start dev server.
+<span class="token function">npm</span> notice
+<span class="token function">npm</span> notice New minor version of <span class="token function">npm</span> available<span class="token operator">!</span> <span class="token number">8.18</span>.0 -<span class="token operator">&gt;</span> <span class="token number">8.19</span>.2
+<span class="token function">npm</span> notice Changelog: https://github.com/npm/cli/releases/tag/v8.19.2
+<span class="token function">npm</span> notice Run <span class="token function">npm</span> <span class="token function">install</span> <span class="token parameter variable">-g</span> npm@8.19.2 to update<span class="token operator">!</span>
+<span class="token function">npm</span> notice
+</code></pre></div><p>\uACB0\uACFC</p><div class="language-text ext-text"><pre class="language-text"><code>$ tree -L 2 -a
+.
+|-- .github
+|   \`-- workflows
+\`-- docs
+    |-- node_modules
+    |-- package-lock.json
+    |-- package.json
+    \`-- src
+</code></pre></div></details><div class="custom-container warning"><p class="custom-container-title">Note</p><p>\uC911\uAC04\uC5D0 \uBCF4\uBA74 \uC124\uCE58 \uACFC\uC815\uC5D0\uC11C <code>?</code>\uB85C \uC2DC\uC791\uD558\uB294, \uC0AC\uC6A9\uC790\uAC00 \uC120\uD0DD \uD639\uC740 \uC785\uB825\uD574\uC57C \uD558\uB294 \uC635\uC158\uB4E4\uC774 \uC8FC\uC5B4\uC9D1\uB2C8\uB2E4.</p><p>\uB9C8\uC9C0\uB9C9 \uC635\uC158\uC73C\uB85C <code>blog</code> \uD0C0\uC785 \uB610\uB294 <code>docs</code> \uD0C0\uC785 \uC911 \uC5B4\uB5A4 \uD0C0\uC785\uC73C\uB85C \uD504\uB85C\uC81D\uD2B8\uB97C \uC0DD\uC131\uD560\uC9C0 \uBB3C\uC5B4\uBCF4\uB294\uB370, <code>blog</code>\uB97C \uC120\uD0DD\uD574\uC57C \uD569\uB2C8\uB2E4.</p></div><h2 id="\u1109\u1175\u11AF\u1112\u1162\u11BC" tabindex="-1"><a class="header-anchor" href="#\u1109\u1175\u11AF\u1112\u1162\u11BC" aria-hidden="true">#</a> \uC2E4\uD589</h2><p>\uC774\uD6C4\uC5D0 <code>docs</code>\uB85C \uB514\uB809\uD1A0\uB9AC\uB97C \uC774\uB3D9\uD55C \uB4A4 <code>npm run docs:dev</code> \uB610\uB294 <code>yarn docs:dev</code> \uBA85\uB839\uC5B4\uB97C \uC2E4\uD589\uD558\uBA74 \uC0DD\uC131\uD55C \uBE14\uB85C\uADF8 \uD504\uB85C\uC81D\uD2B8\uB97C \uAD6C\uB3D9\uD55C \uD654\uBA74\uC744 \uBCFC \uC218 \uC788\uC2B5\uB2C8\uB2E4.</p><div class="language-text ext-text"><pre class="language-text"><code>$ cd docs
+$ yarn docs:dev
+yarn run v1.22.19
+$ vuepress dev src
+\u2714 Initializing and preparing data - done in 397ms
+
+  vite v3.0.9 dev server running at:
+
+  \u279C  Local:   http://localhost:8080/
+  \u279C  Network: http://192.168.35.59:8080/
+</code></pre></div><br><p class="align-center"><img src="https://drive.google.com/uc?export=view&amp;id=1uy8Ss0ot6WBT97s2q99Os2vy1gAx2W5C" alt="" loading="lazy"> &lt;\uD654\uBA74 1. \uD504\uB85C\uC81D\uD2B8 \uAD6C\uB3D9 \uD654\uBA74&gt;</p><h3 id="vuepress-\u1106\u1167\u11BC\u1105\u1167\u11BC\u110B\u1165\u110B\u116A-\u1111\u1169\u11AF\u1103\u1165-\u110B\u1175\u1105\u1173\u11B7-\u1107\u1167\u11AB\u1100\u1167\u11BC" tabindex="-1"><a class="header-anchor" href="#vuepress-\u1106\u1167\u11BC\u1105\u1167\u11BC\u110B\u1165\u110B\u116A-\u1111\u1169\u11AF\u1103\u1165-\u110B\u1175\u1105\u1173\u11B7-\u1107\u1167\u11AB\u1100\u1167\u11BC" aria-hidden="true">#</a> VuePress \uBA85\uB839\uC5B4\uC640 \uD3F4\uB354 \uC774\uB984 \uBCC0\uACBD</h3><p><code>docs</code> \uD3F4\uB354 \uD558\uC704 <code>src</code> \uD3F4\uB354\uC5D0\uB294 \uB2E4\uC74C\uACFC \uAC19\uC740 \uD30C\uC77C\uB4E4\uC774 \uC788\uC2B5\uB2C8\uB2E4.</p><div class="language-text ext-text"><pre class="language-text"><code>$ tree -L 1 -a
+.
+|-- .vuepress
+|-- README.md
+|-- demo
+|-- intro.md
+|-- posts
+|-- slides.md
+\`-- zh
+</code></pre></div><p>\uC5EC\uAE30\uC11C <code>.vuepress</code>\uD3F4\uB354\uAC00 \uBC14\uB85C VuePress \uD504\uB85C\uC81D\uD2B8\uC784\uC744 \uBA85\uC2DC\uD558\uB294 \uD3F4\uB354\uC774\uBA70 \uD574\uB2F9 \uD3F4\uB354\uC5D0\uB294 \uC804\uC5ED \uC124\uC815, \uCEF4\uD3EC\uB10C\uD2B8, \uC815\uC801 \uC790\uC6D0 \uB4F1\uC774 \uC800\uC7A5\uB418\uC5B4 \uC788\uC2B5\uB2C8\uB2E4.</p><p>\uD55C \uD3B8 <code>docs</code> \uD3F4\uB354 \uD558\uC704\uC758 <code>package.json</code> \uD30C\uC77C\uC744 \uC0B4\uD3B4\uBCF4\uBA74 vuepress \uC2E4\uD589 \uBA85\uB839\uC5B4\uC640 \uAD00\uB828\uB41C \uC2A4\uD06C\uB9BD\uD2B8\uB97C \uC0B4\uD3B4\uBCFC \uC218 \uC788\uC2B5\uB2C8\uB2E4.</p><details class="custom-container details"><summary>docs/package.json</summary><div class="language-json ext-json"><pre class="language-json"><code><span class="token punctuation">{</span>
+  ...
+  <span class="token property">&quot;scripts&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+    <span class="token property">&quot;docs:build&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vuepress build src&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;docs:clean-dev&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vuepress dev src --clean-cache&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;docs:dev&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vuepress dev src&quot;</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  ...
+<span class="token punctuation">}</span>
+</code></pre></div></details><p>\uC704\uC640 \uAC19\uC740 \uC0AC\uC2E4\uB4E4\uB85C \uBBF8\uB8E8\uC5B4\uBCF4\uC544 <code>.vuepress</code>\uAC00 \uC704\uCE58\uD55C \uD3F4\uB354\uC758 \uC704\uCE58\uB97C vuepress \uBA85\uB839\uC5B4\uC5D0 \uC804\uB2EC\uD558\uC5EC \uAD6C\uB3D9/\uBE4C\uB4DC\uD558\uB294 \uAC83 \uAC19\uC2B5\uB2C8\uB2E4. \uB530\uB77C\uC11C <code>.vuepress</code>\uAC00 \uC704\uCE58\uD55C \uD3F4\uB354 <code>src</code>\uC758 \uC774\uB984\uC744 \uC784\uC758\uB85C \uBCC0\uACBD\uC774 \uAC00\uB2A5\uD558\uBA70, \uADF8\uC5D0 \uB530\uB77C scripts\uC758 \uBA85\uB839\uC5B4 \uC778\uC790\uB3C4 \uBCC0\uACBD\uB41C \uD3F4\uB354 \uC774\uB984\uC73C\uB85C \uC218\uC815\uD574\uC8FC\uB294 \uBC29\uC2DD\uC73C\uB85C \uD504\uB85C\uC81D\uD2B8 \uAD6C\uC870\uB97C \uBCC0\uACBD\uD560 \uC218 \uC788\uC2B5\uB2C8\uB2E4.</p><p>\uC800 \uAC19\uC740 \uACBD\uC6B0 \uBCF8 \uBE14\uB85C\uADF8 Maemi&#39;s Devlog \uD504\uB85C\uC81D\uD2B8 \uAD6C\uC870\uB97C \uB2E4\uC74C\uACFC \uAC19\uC774 \uC0AC\uC6A9\uD558\uACE0 \uC788\uC2B5\uB2C8\uB2E4.</p><details class="custom-container details"><summary>Maemi&#39;s Devlog \uD504\uB85C\uC81D\uD2B8 \uAD6C\uC870</summary><div class="language-text ext-text"><pre class="language-text"><code>$ tree -L 2 -I &quot;node_modules|.git*&quot; -a
+.
+|-- README.md
+|-- docs
+|   |-- .vuepress
+|   |-- README.md
+|   |-- algorithm
+|   |-- backend
+|   |-- cloud
+|   |-- computer-science
+|   |-- demo
+|   |-- dev-ops
+|   |-- frontend
+|   |-- other
+|   |-- programming-language
+|   \`-- project
+|-- package-lock.json
+\`-- package.json
+</code></pre></div><details class="custom-container details"><summary>package.json</summary><div class="language-json ext-json"><pre class="language-json"><code><span class="token punctuation">{</span>
+  ...
+    <span class="token property">&quot;scripts&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+    <span class="token property">&quot;docs:build&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vuepress build docs&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;docs:clean-dev&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vuepress dev docs --clean-cache&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;docs:dev&quot;</span><span class="token operator">:</span> <span class="token string">&quot;vuepress dev docs&quot;</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  ...
+<span class="token punctuation">}</span>
+</code></pre></div></details></details><h2 id="a-\u110E\u1161\u11B7\u110C\u1169" tabindex="-1"><a class="header-anchor" href="#a-\u110E\u1161\u11B7\u110C\u1169" aria-hidden="true">#</a> A. \uCC38\uC870</h2>`,24),m={href:"http://vuepress-theme-hope.github.io",target:"_blank",rel:"noopener noreferrer"},h={href:"https://vuepress-theme-hope.github.io/v2/cookbook/tutorial/env.html",target:"_blank",rel:"noopener noreferrer"},g=C({__name:"02-create-vuepress-theme-hope-project.html",setup(v){return(E,f)=>{const s=B("ExternalLinkIcon");return o(),t("div",null,[l,d,u("p",null,[u("a",i,[n("Node.js LTS \uACF5\uC2DD \uB2E4\uC6B4\uB85C\uB4DC \uB9C1\uD06C"),e(s)])]),D,u("p",null,[u("a",A,[n("VS Code \uACF5\uC2DD \uB2E4\uC6B4\uB85C\uB4DC \uB9C1\uD06C"),e(s)])]),k,u("p",null,[n('vuepress-theme-hope, "Setting Environment," '),u("em",null,[u("a",m,[n("vuepress-theme-hope.github.io"),e(s)])]),n(", Sep. 8, 2022. [Online]. Available: "),u("a",h,[n("https://vuepress-theme-hope.github.io/v2/cookbook/tutorial/env.html"),e(s)]),n(" [Accessed Oct. 19, 2022].")]),e(p(a))])}}}),x=r(g,[["__file","02-create-vuepress-theme-hope-project.html.vue"]]);export{x as default};
